ed3b3870e39accc6a6fbcd0268723e536b1c7f92
grf5 <g.robinson@f5.com>
2018-06-12 02:39:44 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+/*
+ * iRulesLX RPC for Arista Directflow entry creation
+ *
+ */
+
+/* Import the f5-nodejs module. */
+var f5 = require('f5-nodejs');
+var directflow = require('./directflow.js');
+
+/* Create a new rpc server for listening to TCL iRule calls. */
+var ilx = new f5.ILXServer();
+ilx.listen();
+
+/**
+  * create directflow entry on Arista EOS device
+  *
+  * @param {String} protocol
+  * @param {String} source_address
+  * @return {Boolean} result
+  */
+ilx.addMethod('createDirectflowEntry', function(req, res) {
+	directflow.createDirectflowEntry(req.params()[0], function(result) {
+		if(typeof result !== undefined) {
+			res.reply(true);
+		} else {
+			res.reply(false);
+		}
+	});
+});
