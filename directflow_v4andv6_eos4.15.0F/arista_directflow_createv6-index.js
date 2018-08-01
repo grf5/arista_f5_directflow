e7339ffcebf7929e66f7dcda1e9ba341c49a9aaa
grf5 <g.robinson@f5.com>
2018-08-01 10:02:34 -0400

    Update arista_directflow_createv6-index.js

---
+++ directflow_v4andv6_eos4.15.0F/arista_directflow_createv6-index.js
@@ -0,0 +1,29 @@
+/*
+ * iRulesLX RPC for Arista Directflow entry creation
+ *
+ */
+
+/* Import the f5-nodejs module. */
+var f5 = require('f5-nodejs');
+var directflow = require('./directflow.js');
+
+/* Create a new rpc server for listening to TCL iRule calls. */
+var ilx = new f5.ILXServer();
+ilx.listen();
+
+/**
+  * create directflow entry on Arista EOS device
+  *
+  * @param {String} protocol
+  * @param {String} source_address
+  * @return {Boolean} result
+  */
+ilx.addMethod('createDirectflowEntryv6', function(req, res) {
+	directflow.createDirectflowEntryv6(req.params()[0], req.params()[1], req.params()[2], req.params()[3], req.params()[4], req.params()[5], function(result) {
+		if(typeof result !== undefined) {
+			res.reply(true);
+		} else {
+			res.reply(false);
+		}
+	});
+});
